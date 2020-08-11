55cccf96baf2d90066643118670ea9820464f83d
Felix <faportillo@ucdavis.edu>
2020-08-10 22:46:08 -0700

    Modified Body and Satellite classes with new parameters

--- body.py
+++ body.py
@@ -1,35 +1,50 @@
 import numpy as np
 from vpython import *
-import astrodynamics as ast
 
 
 class Body(object):
-    def __init__(self, name, mass, dimensions, orbital_info, velocity_0=None, altitude_0=1000, body_color=None,
-                 texture=None):
+    def __init__(self, name, mass, dimensions, position=None, orbital_info=None, body_type='planet', velocity_0=None,
+                 altitude_0=1000, body_color=None, texture=None):
         """
             Initialize the Body class. Will be assigned initial position at the ascending node based on orbital info.
             :param name: string name of Body
             :param mass: the body's mass in kg
             :param dimensions: the body's dimensions as a list of max size 3. If size is 1, spherical object. Size of 2
                 will be oblong sphere object, size of 3 will be cube object (typically for satellites)
-            :param altitude_0: The initial altitude of the body
+            :param position: The initial position
             :param orbital_info: Dict with orbital characteristics {semimajor_axis, eccentricity, inclination,
                 right_ascension, argument_perigee}
+            :param body_type: Type of body {'planet', 'satellite'}
+            :param velocity_0: Initial velocity vector of the body, defaults to None
+            :param altitude_0: The initial altitude of the body
+            :param body_color: The body's color, random if not specified
+            :param texture: Can set body to have a texture instead of a color
         """
         self.name = name
         self.mass = mass
         self.dimensions = dimensions
         self.orbital_info = orbital_info
         self.altitude_0 = altitude_0
-        self.position = (self.altitude_0 * np.cos(self.orbital_info['right_ascension']),
-                         self.altitude_0 * np.cos(self.orbital_info['right_ascension']), 0)
+        if position is not None:
+            self.position = position
+        elif orbital_info is not None:  # If initial position not specified, then assume it's orbiting body
+            self.position = (self.altitude_0 * np.cos(self.orbital_info['right_ascension']),
+                             self.altitude_0 * np.cos(self.orbital_info['right_ascension']), 0)
+
         self.velocity = velocity_0
 
-        self.body = sphere(pos=self.position,
-                           size=self.dimensions,
-                           color=body_color if body_color is not None else vector.random(),
-                           make_trail=True,
-                           texture=texture)
+        if body_type == 'planet':
+            self.body = sphere(pos=self.position,
+                               size=self.dimensions,
+                               color=body_color if body_color is not None else vector.random(),
+                               make_trail=True,
+                               texture=texture)
+        elif body_type == 'satellite':
+            self.body = box(pos=self.position,
+                            length=self.dimensions[0], height=self.dimensions[1], width=self.dimensions[2],
+                            color=body_color if body_color is not None else vector.random(),
+                            make_trail=True,
+                            texture=texture)
 
     def update_body(self, a, velocity_vector, dt):
         """
--- satellite.py
+++ satellite.py
@@ -4,5 +4,21 @@ from body import Body
 
 
 class Satellite(Body):
-    def __init__(self, altitude):
-        self.altitude = altitude
+    def __init__(self, name, mass, dimensions, position=None, orbital_info=None, body_type='planet', velocity_0=None,
+                 altitude_0=1000, body_color=None, texture=None):
+        """
+            Satellite class
+            :param name: string name of Body
+            :param mass: the body's mass in kg
+            :param dimensions: the body's dimensions as a list of max size 3. If size is 1, spherical object. Size of 2
+                will be oblong sphere object, size of 3 will be cube object (typically for satellites)
+            :param orbital_info: Dict with orbital characteristics {semimajor_axis, eccentricity, inclination,
+                right_ascension, argument_perigee}
+            :param body_type: Type of body {'planet', 'satellite'}
+            :param velocity_0: Initial velocity vector of the body, defaults to None
+            :param altitude_0: The initial altitude of the body
+            :param body_color: The body's color, random if not specified
+            :param texture: Can set body to have a texture instead of a color
+        """
+        super().__init__(name, mass, dimensions, position, orbital_info, body_type, velocity_0,
+                         altitude_0, body_color, texture)
